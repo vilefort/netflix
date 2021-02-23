a733c0f4c5390650dd53a58913e16f10f43c669a
vilefort <vilefort@gmail.com>
2021-02-23 18:44:24 -0300

    Netflix Prime
    Repaging site Netflix

---
+++ js/owl/setup.js
@@ -0,0 +1,18 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    nav: false,
+    autoWidth: true, 
+  
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:5
+        },
+        1000:{
+            items:8
+        }
+    }
+})
+
